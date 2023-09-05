d6351a1b04200186555709610e16899186f31ab7
Caroline1883 <36411250+Caroline1883@users.noreply.github.com>
2023-09-05 10:15:26 +0200

    Initialisation CSS

--- functions.php
+++ functions.php
@@ -1,8 +1,7 @@
 <?php
 require_once get_template_directory() . '/menus.php';
 function mota_enqueue_styles() {
-    // Styles
-        // wp_enqueue_style('x', get_stylesheet_directory_uri() . '/x.css');
+        wp_enqueue_style('main', get_template_directory_uri() . '/css/main.css');
     // Scripts
         // wp_enqueue_script('x', get_stylesheet_directory_uri() . '/x.js', array('jquery'), '1.0', true);
 }
@@ -13,3 +12,13 @@ function register_menu() {
     register_nav_menu('main-menu', __('Menu Principal', 'motaphoto'));
 }
 add_action('after_setup_theme', 'register_menu');
+
+// Adds
+add_theme_support( 
+    'custom-logo',
+    array(
+    'flex-height'          => true,
+    'flex-width'           => true,
+    'unlink-homepage-logo' => true, 
+    )
+    );
