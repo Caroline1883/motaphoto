e68512e069a3c00d9f3268f92adc922393096b4c
Caroline1883 <36411250+Caroline1883@users.noreply.github.com>
2023-09-06 00:15:56 +0200

    Structure HTML Modale OK

--- template-parts/contact.php
+++ template-parts/contact.php
@@ -20,7 +20,7 @@
 	<div class="popup-contact">
 		<div class="popup-header">
 			<img src="<?php echo get_template_directory_uri() . '/assets/img/contactheader.svg'; ?>" alt="contact contact contact">
-			<span class="popup-close"><i class="fa fa-times"></i></span>
+			<span class="popup-close">&#10006</span>
 		</div>
 		<div class="popup-form">
 		<?php
