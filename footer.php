a85bb9402417dfd02360eab352a322ec5b0d1288
Caroline1883 <36411250+Caroline1883@users.noreply.github.com>
2023-09-05 23:18:22 +0200

    Ajout du menu footer

--- footer.php
+++ footer.php
@@ -1,3 +1,14 @@
 <?php wp_footer(); ?>
+<footer>
+    <nav>
+        <?php
+            wp_nav_menu([
+                'theme_location' => 'footer-menu',
+                'container'      => false, // without WP container
+                'menu_class'     => 'desktop-footer',    
+                ]);
+            ?>
+    </nav>
+</footer>
 </body>
 </html>
