1a19841cd48c4f95ccbfa5d0da5896fbb961464b
Caroline1883 <36411250+Caroline1883@users.noreply.github.com>
2023-09-06 22:46:34 +0200

    Après mentorat 1

--- single-single-photo.php
+++ single-single-photo.php
@@ -1,5 +1,5 @@
 <!-- /**
- * The template for displaying all single publication
+ * The template for displaying all single photo posts
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
@@ -7,57 +7,11 @@
  * @subpackage Motaphoto
  * @since Motaphoto 1.0
  */ -->
- <?php get_header(); ?>
- <?php the_terms( $post->ID, 'single-photo', 'Type : '); ?>
- <?php
-// Pour plus tard ACF
-?>
 
+ <?php get_header(); ?>
+ <h1>test</h1>
  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
- <?php get_template_part( 'entry' ); ?>
- <?php if ( comments_open() && !post_password_required() ) { comments_template( '', true ); } ?>
+ <!-- Si jamais il n'y a pas de script entry-photo alors entry  -->
+ <?php get_template_part( 'template-parts/entry', 'photo' ); ?>
  <?php endwhile; endif; ?>
- <footer class="footer">
- <?php get_template_part( 'nav', 'below-single' ); ?>
- </footer>
  <?php get_footer(); ?>
-
- <?php get_header(); ?>
-
-<div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
-
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <header class="entry-header">
-                    <h1 class="entry-title"><?php the_title(); ?></h1>
-                </header>
-
-                <div class="entry-content">
-                    <?php
-                    if (has_post_thumbnail()) {
-                        the_post_thumbnail('large'); // Affiche l'image à la taille "large"
-                    }
-                    ?>
-                    <?php the_content(); ?>
-                </div>
-
-                <footer class="entry-footer">
-                    <?php
-                    // Afficher ici vos taxonomies personnalisées (par exemple, 'taxonomy_name')
-                    // echo get_the_term_list($post->ID, 'taxonomy_name', '<p>Les catégories : ', ', ', '</p>');
-                    ?>
-                </footer>
-            </article>
-
-            <?php if (comments_open() || get_comments_number()) : ?>
-                <?php comments_template(); ?>
-            <?php endif; ?>
-
-        <?php endwhile; endif; ?>
-
-    </main><!-- #main -->
-</div><!-- #primary -->
-
-<?php get_footer(); ?>
---
+++ template-parts/entry-photo.php
@@ -0,0 +1,9 @@
+<?php 
+    the_title('<h1>','</h1>');
+    the_content();
+    the_excerpt();
+    the_tags();
+    the_category();
+    the_post_thumbnail();
+    
+ ?>
