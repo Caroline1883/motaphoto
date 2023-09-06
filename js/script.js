bca90ba450c7cf1d148ff1ef638f2660e4d617d0
Caroline1883 <36411250+Caroline1883@users.noreply.github.com>
2023-09-06 23:38:03 +0200

    Fonction JS simplifiée

--- js/script.js
+++ js/script.js
@@ -1,59 +1,55 @@
-console.log("coucou")
-
-// Header
-
-const menuToggle = document.querySelector('.hamburger');
-const menuCross = document.querySelector('.cross');
-const fullScreen = document.querySelector('.fullscreen');
-const menuLis = document.querySelectorAll('li');
-
-document.addEventListener('DOMContentLoaded', function () {
-    menuToggle.addEventListener('click', toggleMenu);
-    menuCross.addEventListener('click', toggleMenu);
-});
-
-function toggleMenu() {
-    fullScreen.classList.toggle('inactive');
-    menuCross.classList.toggle('inactive');
-    menuToggle.classList.toggle('inactive');
-}
-
-function menuLiF() {
-    fullScreen.classList.toggle('active');
-    menuToggle.classList.toggle('active');
-}
-
-menuLis.forEach(menuLi => {
-    menuLi.addEventListener('click', menuLiF);
-});
-
-// Modal Contact
+// Header + Modale
 
 document.addEventListener('DOMContentLoaded', function() {
-    
-const modalOverlay = document.querySelector('.popup-overlay');
-const menuContact = document.querySelectorAll('.contact');
-const fullScreen = document.querySelector('.fullscreen');
-const menuCross = document.querySelector('.cross');
-const menuToggle = document.querySelector('.hamburger');
-menuContact.forEach(item => {
-    item.onclick = function() {
-        modalOverlay.style.display = "block";
-        fullScreen.classList.add('inactive');
-        menuCross.classList.add('inactive');
-        menuToggle.classList.remove('inactive');
+
+    const menuToggle = document.querySelector('.hamburger');
+    const menuCross = document.querySelector('.cross');
+    const fullScreen = document.querySelector('.fullscreen');
+    const menuLis = document.querySelectorAll('li')
+    const modalOverlay = document.querySelector('.popup-overlay');
+    const menuContact = document.querySelectorAll('.contact');
+
+    menuToggle.addEventListener('click', toggleMenu);
+    menuCross.addEventListener('click', toggleMenu);
+       menuContact.forEach(item => {
+            item.onclick = function() {
+                modalOverlay.style.display = "block";
+                fullScreen.classList.add('inactive');
+                menuCross.classList.add('inactive');
+                menuToggle.classList.remove('inactive');
+               }
+            });
+
+    window.onclick = function(event) {
+        if (event.target == modalOverlay) {
+            modalOverlay.style.display = "none";
+            }
+        }
         
-    }
-})
-console.log(menuContact);
+    function toggleMenu() {
+        fullScreen.classList.toggle('inactive');
+        menuCross.classList.toggle('inactive');
+        menuToggle.classList.toggle('inactive');
+        }
+        
+        function menuLiF() {
+        fullScreen.classList.toggle('active');
+        menuToggle.classList.toggle('active');
+        }
+        
+        menuLis.forEach(menuLi => {
+        menuLi.addEventListener('click', menuLiF);
+        });
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modalOverlay) {
-        modalOverlay.style.display = "none";
     }
-}
+);
 
-});
+
+
+
+
+
+
+
 
 
