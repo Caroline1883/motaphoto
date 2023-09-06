940d9940bc8aac045f1a06d319b6e43cf3041640
Caroline1883 <36411250+Caroline1883@users.noreply.github.com>
2023-09-07 00:05:25 +0200

    Récupération valeurs ACF OK

--- template-parts/entry-photo.php
+++ template-parts/entry-photo.php
@@ -5,5 +5,21 @@
     the_tags();
     the_category();
     the_post_thumbnail();
+    the_field('ref');
+    the_field('file');
+    the_field('title');
+    the_field('category');
+    the_field('format');
+    the_field('type');
+    the_field('ref');
     
+
+
  ?>
+
+    <!-- [[acf field="file"]]
+    [[acf field="title"]]
+    [[acf field="ref"]]
+    [[acf field="category"]]
+    [[acf field="format"]]
+    [[acf field="type"]] -->
