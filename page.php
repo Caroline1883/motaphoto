244345e6f577a29cc28f08bca90d639b228a6243
Caroline1883 <36411250+Caroline1883@users.noreply.github.com>
2023-09-06 16:05:59 +0200

    Initialisation single template

--- page.php
+++ page.php
@@ -1,25 +1,15 @@
 <?php get_header(); ?>
-
-<div id="primary" class="content-area">
-    <main id="main" class="site-main">
-        <?php
-        while (have_posts()) :
-            the_post();
-            ?>
-
-            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <header class="entry-header">
-                    <h1 class="entry-title"><?php the_title(); ?></h1>
-                </header>
-
-                <div class="entry-content">
-                    <?php the_content(); ?>
-                </div>
-            </article>
-
-        <?php endwhile; ?>
-    </main>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<header class="header">
+<h1 class="entry-title" itemprop="name"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
+</header>
+<div class="entry-content" itemprop="mainContentOfPage">
+<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'full', array( 'itemprop' => 'image' ) ); } ?>
+<?php the_content(); ?>
+<div class="entry-links"><?php wp_link_pages(); ?></div>
 </div>
-
-<?php get_sidebar(); ?>
+</article>
+<?php if ( comments_open() && !post_password_required() ) { comments_template( '', true ); } ?>
+<?php endwhile; endif; ?>
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,4 +1,3 @@
-<?php 
 /**
  * The template for displaying all single posts
  *
@@ -9,42 +8,12 @@
  * @since Motaphoto 1.0
  */
 
-get_header(); ?>
-<div id="primary" class="content-area">
-    <main id="main" class="site-main">
-        <?php
-        while (have_posts()) :
-            the_post();
-            ?>
-
-            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <header class="entry-header">
-                    <h1 class="entry-title"><?php the_title(); ?></h1>
-                </header>
-
-                <div class="entry-content">
-                    <?php the_content(); ?>
-                </div>
-
-                <footer class="entry-footer">
-                    <?php
-                    if (has_category() || has_tag()) {
-                        echo '<div class="entry-meta">';
-                        if (has_category()) {
-                            echo '<span class="cat-links">' . __('Posted in', 'yourtheme') . ': ' . get_the_category_list(', ') . '</span>';
-                        }
-                        if (has_tag()) {
-                            echo '<span class="tags-links">' . __('Tagged', 'yourtheme') . ': ' . get_the_tag_list('', ', ') . '</span>';
-                        }
-                        echo '</div>';
-                    }
-                    ?>
-                </footer>
-            </article>
-
-        <?php endwhile; ?>
-    </main>
-</div>
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+ <?php get_header(); ?>
+ <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+ <?php get_template_part( 'entry' ); ?>
+ <?php if ( comments_open() && !post_password_required() ) { comments_template( '', true ); } ?>
+ <?php endwhile; endif; ?>
+ <footer class="footer">
+ <?php get_template_part( 'nav', 'below-single' ); ?>
+ </footer>
+ <?php get_footer(); ?>
