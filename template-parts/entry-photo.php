6f2cca0ae4c7455ef35d2e5a86256be643f687d1
Caroline1883 <36411250+Caroline1883@users.noreply.github.com>
2023-09-12 08:22:57 +0200

    Complétion HTML Single Page

--- template-parts/entry-photo.php
+++ template-parts/entry-photo.php
@@ -3,15 +3,45 @@
     <div class="meta">
       <h1><?= the_title('<h1>', '</h1>'); ?></h1>
       <ul>
-         <li><h4>Référence : <?= the_field('ref'); ?></h4></li>
-         <li><h4>Format : <?= the_field('format'); ?></h4></li>
-         <li><h4>Catégorie : <?= the_field('cat'); ?></h4></li>
+        <li><h4>Référence : <?= get_field('ref');?></h4></li>
+        <li><h4>Format : 
+          <?php
+          $format_id = get_field('format');
+          if ($format_id) {
+            $format_term = get_term($format_id); 
+            if (is_object($format_term) && property_exists($format_term, 'name')) {
+                $format_label = $format_term->name;
+                echo esc_html($format_label);
+            } else {
+                echo 'Aucune référence définie';
+            }
+        } else {
+            echo 'Aucune référence définie'; 
+        } 
+          ?>
+        </h4></li>
+         <li><h4>Catégorie :
+          <?php 
+            $photocat_id = get_field('photocat');
+            if ($photocat_id) {
+              $photocat_term = get_term($photocat_id); 
+              if (is_object($photocat_term) && property_exists($photocat_term, 'name')) {
+                  $photocat_label = $photocat_term->name;
+                  echo esc_html($photocat_label);
+              } else {
+                  echo 'Aucune référence définie';
+              }
+          } else {
+              echo 'Aucune référence définie'; 
+          }
+          ?>
+         </h4></li>
          <li><h4>Type : <?= the_field('type'); ?></h4></li>
          <li><h4>Année : <?= the_date('Y'); ?></h4></li>
       </ul>
     </div>
     <div class="pic">
-      <img src="<?= the_field('file'); ?>" alt="<?= the_field('file'); ?>">
+      <img src="<?= the_field('file'); ?>" alt="<?= the_field('description'); ?>">
     </div>
     <div class="contact">
       <span>Cette photo vous intéresse ?</span>
