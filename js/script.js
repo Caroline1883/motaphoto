7087a0660c1e3f38efcf31929e026286738ac0b5
Caroline1883 <36411250+Caroline1883@users.noreply.github.com>
2023-09-14 12:08:13 +0200

    Nav KO

--- js/script.js
+++ js/script.js
@@ -45,58 +45,7 @@ document.addEventListener('DOMContentLoaded', function() {
 );
 
 // Single photo navigation
-
-document.addEventListener('DOMContentLoaded', function () {
-    const prevThumbnail = document.querySelector('.photoleft');
-    const nextThumbnail = document.querySelector('.photoright');
-    const prevArrow = document.querySelector('.navleft');
-    const nextArrow = document.querySelector('navright');
-  
-    // hover
-    function handleHover(e) {
-      prevThumbnail.style.display = 'none';
-      nextThumbnail.style.display = 'none';
-  
-      if (e.target === prevArrow) {
-        prevThumbnail.style.display = 'inline-block';
-      } else if (e.target === nextArrow) {
-        nextThumbnail.style.display = 'inline-block';
-      }
-    }
-  
-    // // navigation
-    // function handleNavigation(e) {
-    //   e.preventDefault();
-  
-    //   const action = e.target.getAttribute('data-action');
-  
-    //   // Effectuez la navigation en fonction de l'action (précédente ou suivante)
-    //   if (action === 'previous') {
-    //     // Code pour aller à la photo précédente
-    //   } else if (action === 'next') {
-    //     // Code pour aller à la photo suivante
-    //   }
-    // }
-  
-    // EventListener
-    prevArrow.addEventListener('mouseenter', handleHover);
-    nextArrow.addEventListener('mouseenter', handleHover);
-  
-    prevArrow.addEventListener('mouseleave', function () {
-      prevThumbnail.style.display = 'none';
-    });
-    nextArrow.addEventListener('mouseleave', function () {
-      nextThumbnail.style.display = 'none';
-    });
-  
-    // prevArrow.addEventListener('click', handleNavigation);
-    // nextArrow.addEventListener('click', handleNavigation);
-  });
-  
-
-
-
-
+// Consigne à revoir
 
 
 
--- template-parts/entry-photo.php
+++ template-parts/entry-photo.php
@@ -111,7 +111,7 @@
 
     <div class="photoarrows">
       <?php if (isset($previous_exist)) { ?>
-        <img class="navleft" src="<?php echo esc_url(get_template_directory_uri()) . '/assets/img/left.png'; ?>" alt="Navigation gauche" data-action="previous">
+        <a><img class="navleft" src="<?php echo esc_url(get_template_directory_uri()) . '/assets/img/left.png'; ?>" alt="Navigation gauche" data-action="previous">
       <?php } ?>
       <?php if (isset($next_exist)) { ?>
         <img class="navright" src="<?php echo esc_url(get_template_directory_uri()) . '/assets/img/right.png'; ?>" alt="Navigation droite" data-action="next">
