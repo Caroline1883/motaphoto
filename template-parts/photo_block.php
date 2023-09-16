166fc9bb9fccff04cab06faa9c39c6b043679372
Caroline1883 <36411250+Caroline1883@users.noreply.github.com>
2023-09-16 22:43:08 +0200

    Essai filtrage upsell

--- template-parts/photo_block.php
+++ template-parts/photo_block.php
@@ -1,53 +1,62 @@
 <?php
 /**
- * Template part created photoblock home & single-photo
-
+ * Template part for displaying photoblock in home & single-photo
  */
 
-//  $terms = wp_get_post_terms(get_the_ID(), 'photocat'); // Remplacez 'votre_taxonomie' par le nom de votre taxonomie
+// Obtenir les termes de la taxonomie 'photocat' associés à l'article actuel
+$terms = wp_get_post_terms(get_the_ID(), 'photocat');
 
-// if ($terms) {
-//     $term_ids = array();
-//     foreach ($terms as $term) {
-//         $term_ids[] = $term->term_id;
-//     }
+if (!empty($terms)) {
+    // Initialiser un tableau pour stocker les IDs des termes
+    $term_ids = array();
 
+    // Parcourir les termes et extraire leurs IDs
+    foreach ($terms as $term) {
+        $term_ids[] = $term->term_id;
+    }
 
- $args_upsell = array(
-    'post_type' => 'single-photo',
-    'posts_per_page' => 2,
-    'orderby' => 'date',
-    'order' => 'ASC',
-    'date_query' => array(
-      'after' => get_the_date('Y-m-d H:i:s'),
-    ),
-    // 'tax_query' => array(
-    //     array(
-    //         'taxonomy' => 'photocat', // Remplacez 'votre_taxonomie' par le nom de votre taxonomie
-    //         'field' => 'id',
-    //         'terms' => $term_ids,
-    //     ),
-    // )
-);
+    // Args de la requête pour récupérer les articles avec les termes de la taxonomie 'photocat'
+    $args_upsell = array(
+        'post_type' => 'single-photo',
+        'posts_per_page' => 2,
+        'orderby' => 'date',
+        'order' => 'ASC',
+        // 'tax_query' => array(
+        //     array(
+        //         'taxonomy' => 'photocat', // Remplacez par le nom de votre taxonomie
+        //         'field' => 'id',
+        //         'terms' => $term_ids,
+        //     ),
+        // ),
+    );
 
-  $my_query = new WP_Query( $args_upsell);
+    // Créer la requête personnalisée
+    $my_query = new WP_Query($args_upsell);
 
-  if($my_query->have_posts()) : while ($my_query->have_posts()) : $my_query->the_post();
-    echo '<div class="photo-container">';
-    echo '<div class="photo_block"><img src="' . esc_url(get_field('file')) . '" alt="' . esc_attr(get_field('description')) . '"></div>';
-    echo '
-    <div class="icons">
-    <span class="info-icon"><i class="fa fa-eye"></i></span>
-    <span class="fullscreen-icon"><i class="fa fa-arrows-alt"></i></span>
-    </div></div>';
-
-endwhile;
-endif;
-
-wp_reset_postdata();
-
+    if ($my_query->have_posts()) :
+        while ($my_query->have_posts()) : $my_query->the_post();
 ?>
+
+            <div class="photo-container">
+                <div class="photo_block">
+                    <img src="<?= esc_url(get_field('file')); ?>" alt="<?= esc_attr(get_field('description')); ?>">
+                </div>
+                <div class="icons">
+                    <span class="info-icon"><i class="fa fa-eye"></i></span>
+                    <span class="fullscreen-icon"><i class="fa fa-arrows-alt"></i></span>
+                </div>
+            </div>
+
+<?php
+        endwhile;
+        wp_reset_postdata();
+    endif;
+} else {
+    echo 'Aucun terme trouvé dans la taxonomie "photocat".';
+}
+?>
+
 </div>
 <div class="load">
-<button class="wpcf7-submit">Toutes les photos</button>
+    <button class="wpcf7-submit">Toutes les photos</button>
 </div>
