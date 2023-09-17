80e0a274603e35c86a3bcddfdd89061db0f0737e
Caroline1883 <36411250+Caroline1883@users.noreply.github.com>
2023-09-17 08:37:15 +0200

    On repart de zéro

--- template-parts/photo_block.php
+++ template-parts/photo_block.php
@@ -3,34 +3,13 @@
  * Template part for displaying photoblock in home & single-photo
  */
 
-// Obtenir les termes de la taxonomie 'photocat' associés à l'article actuel
-$terms = wp_get_post_terms(get_the_ID(), 'photocat');
-
-if (!empty($terms)) {
-    // Initialiser un tableau pour stocker les IDs des termes
-    $term_ids = array();
-
-    // Parcourir les termes et extraire leurs IDs
-    foreach ($terms as $term) {
-        $term_ids[] = $term->term_id;
-    }
-
-    // Args de la requête pour récupérer les articles avec les termes de la taxonomie 'photocat'
     $args_upsell = array(
         'post_type' => 'single-photo',
         'posts_per_page' => 2,
         'orderby' => 'date',
         'order' => 'ASC',
-        // 'tax_query' => array(
-        //     array(
-        //         'taxonomy' => 'photocat', // Remplacez par le nom de votre taxonomie
-        //         'field' => 'id',
-        //         'terms' => $term_ids,
-        //     ),
-        // ),
     );
 
-    // Créer la requête personnalisée
     $my_query = new WP_Query($args_upsell);
 
     if ($my_query->have_posts()) :
@@ -51,9 +30,7 @@ if (!empty($terms)) {
         endwhile;
         wp_reset_postdata();
     endif;
-} else {
-    echo 'Aucun terme trouvé dans la taxonomie "photocat".';
-}
+
 ?>
 
 </div>
